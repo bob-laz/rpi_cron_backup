d7c3ae9c34e4ff2d61a2ac9f12ef8cc621e3fe22
Bob Laskowski <blaskowski94@gmail.com>
2022-06-19 18:08:22 -0700

    update status script to send back online email

--- status_script.sh
+++ status_script.sh
@@ -1,11 +1,16 @@
 TO_EMAIL="blaskowski94@gmail.com"
 IP=192.168.0.20
+status=`cat status.txt`
 
 ping $IP -c 3
 
 if [ $? -eq 0 ]; then
    	echo "$IP rpi online"
-   	echo "ONLINE" > status.txt
+	if [ "$status" != "ONLINE" ]; then
+		echo "sending back online notification"
+   		echo "ONLINE" > status.txt
+     	echo "$IP rpi is back online" | mail -s "$IP was offline but is back online now" $TO_EMAIL
+   	fi
    	currenttime=$(date +%H:%M)
   	if [[ "$currenttime" < "00:01" ]]; then
      	echo 'sending daily all is well email'
@@ -13,7 +18,6 @@ if [ $? -eq 0 ]; then
     fi
 else
     echo "STATUS JOB FAILED, $IP IS OFFLINE"
-    status=`cat status.txt`
 	if [ "$status" != "OFFLINE" ]; then
 		echo "sending offline notification"
 		echo "OFFLINE" > status.txt
